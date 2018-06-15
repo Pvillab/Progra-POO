ac87a24cc27754d2dbacfea84ddb86487160ac3b
Pvillab <39679699+Pvillab@users.noreply.github.com>
2018-06-14 18:51:30 -0600

    Se supone que aqui esta bueno
    No me quiero ir señor Marcelo ;v

--- AplicacionAdministradora/src/controller/Server.java
+++ AplicacionAdministradora/src/controller/Server.java
@@ -67,13 +67,6 @@ public class Server {
             
         }
        
-        
-        
-        
-        
-        
-        
-          
     }
     
     public void nuevoPlatillo(Plato miPlato){
